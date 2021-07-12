103285d56d1d052b3af57bb2194de146c94bf88d
lukehaines2 <lukehaines2@gmail.com>
2021-07-12 18:13:13 +0100

    iterate and create nodes with JS

---
+++ index.js
@@ -0,0 +1,9 @@
+const hexagonLi = document.getElementsByTagName('li');
+console.log('L2 === hexagonLi', hexagonLi);
+
+const hexagonChild = document.getElementsByClassName('hexagonli');
+// Iterate to create 12 li's to create the grid
+for (var i = 0; i < 12; i++) {
+  let newClone = hexagonChild.cloneNode(true);
+  // console.log(i)
+}
